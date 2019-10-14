13875bd193ac809e9863b171dc0167a5d890a761
Samantha Phipps <sammi.phipps@gmail.com>
2019-10-14 22:28:58 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,8 +1,10 @@
 class Owner
-  attr_reader :name
+  attr_reader :name, :species
   def initialize(name)
     @name = name 
     @species = "human"
   end 
-  def say
+  def say_species
+    "I am #{@species}"
+  end 
 end
